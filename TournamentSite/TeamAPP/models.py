f4359a2ca9d34ba7035ecf952cf4c74a301345e9
DYSIMCE2 <markosimeonov1@gmail.com>
2023-03-25 11:20:01 +0100

    Team and Player models

--- TournamentSite/TeamAPP/models.py
+++ TournamentSite/TeamAPP/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+   name= models.CharField(max_length=15)
+   colors= models.CharField(max_length=15)
+    
+    
+class Player(models.Model):
+    firstname= models.CharField(max_length=15)
+    lastname= models.CharField(max_length=15)
+    age= models.IntegerField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
--- TournamentSite/TeamAPP/views.py
+++ TournamentSite/TeamAPP/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Player
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World welcome to my tournament page")
+def details(request):
+    players =Player.objects.all()
+    players = [
+        f"{player.firstname} {player.lastname} " for player in players
+    ]
+    return HttpResponse(players)
